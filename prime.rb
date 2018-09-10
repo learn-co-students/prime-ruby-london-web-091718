46bcc185d376baf214ab47367a580a2e92dd1e07
Ryan Barker <ryangeorgebarker0@gmail.com>
2018-09-10 15:50:33 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
-# Add  code here!
+def prime?(number)
+  if number < 2
+    return false
+  else
+    (2..number - 1).to_a.all? do |check_prime|
+      number % check_prime != 0
+    end
+  end
+end
